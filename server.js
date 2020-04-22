f610972ae69c302b6b82ea59f30486cf8a0470ce
Kelvin <kelvin.martinez92@gmail.com>
2020-04-22 15:16:10 -0400

    server file updated with some code to get back end connected

--- server.js
+++ server.js
@@ -8,29 +8,32 @@ var PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var databaseToUse = "mongodb://127.0.0.1:27017/todos";
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   databaseToUse =
-//     "mongodb://gabe:a123456@ds129010.mlab.com:29010/heroku_qpsq8tsw";
-// } else {
-//   databaseToUse = "mongodb://127.0.0.1:27017/todos";
-// }
+var databaseToUse = "";
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  databaseToUse =
+    "mongodb://gabe:a123456@ds129010.mlab.com:29010/heroku_qpsq8tsw";
+} else {
+  databaseToUse = "mongodb://127.0.0.1:27017/todos";
+}
 
 
 
 
 app.use(routes);
 
-const MONGODB_URI = databaseToUse;
+const MONGODB_URI = process.env.MONGODB_URI || databaseToUse;
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(MONGODB_URI);
 
+
+
 app.listen(PORT, function () {
-  console.log(`App running on port ${PORT}`);
+  console.log("App is running on port:" + PORT)
 });
 
 
