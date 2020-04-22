8e869e63227b12d06c7b706c690f8e41d82c6b1f
Kelvin <kelvin.martinez92@gmail.com>
2020-04-22 09:47:08 -0400

    set up server file correctly

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-const router = require('express').Router();
-const db = require('todo.model.js');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
 
-module.exports = router;
-//route to create Fruit
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var databaseToUse = "";
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  databaseToUse =
+    "mongodb://gabe:a123456@ds129010.mlab.com:29010/heroku_qpsq8tsw";
+} else {
+  databaseToUse = "mongodb://localhost/reactBoilerplate";
+}
 
 
-//route to get Fruits
-router.get('/api/fruit', (req, res) => {
-	db.Fruit
-		.find({})
-		.then((fruits) => {
-			res.json(fruits);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+
+
+app.use(routes);
+
+const MONGODB_URI = process.env.MONGODB_URI || databaseToUse;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function () {
+  console.log(`App running on port ${PORT}`);
 });
+
+
 
 
